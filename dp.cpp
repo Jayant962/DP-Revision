3afc09a7767dc1244d4fc913e7957dbdfdeab943
Jayant962 <jayantdhingra370@gmail.com>
2026-04-14 20:34:39 +0530

    Min. coin to achieve target amount

--- dp.cpp
+++ dp.cpp
@@ -105,3 +105,60 @@ int countDistinctWays(int nStairs) {
     vector<int> dp(nStairs+1,-1);
     return solve(nStairs,dp);
 }
+
+
+
+
+
+/////////////////  nth element (min. no. of coins)
+
+/// Recursion
+int solve(vector<int> num,int x){
+    if(x==0) return 0;
+    if(x<0) return INT_MAX;
+
+    int mini = INT_MAX;
+    for(int i=0;i<num.size();i++){
+        int ans = solve(num,x-num[i]);
+        if(ans!=INT_MAX){
+            mini = min(mini,1+ans);
+        }
+    }
+    return mini;
+}
+
+/// Recursion+Memoization
+int solve1(vector<int> num,int x,vector<int>& dp){
+    if(x==0) return 0;
+    if(x<0) return INT_MAX;
+
+    if(dp[x]!=-1) return dp[x];
+
+    int mini = INT_MAX;
+    for(int i=0;i<num.size();i++){
+        int ans = solve(num,x-num[i]);
+        if(ans!=INT_MAX){
+            mini = min(mini,1+ans);
+        }
+    }
+    dp[x] = mini;
+    return mini;
+}
+
+/// Tabulation
+int solve2(vector<int> num,int x){
+    vector<int> dp(x+1,INT_MAX);
+    dp[0] = 0;
+
+    for(int i=1;i<=x;i++){
+        for(int j=0;j<num.size();j++){
+            if(i-num[j] >= 0 && dp[i-num[j]]!=INT_MAX){
+                dp[i] = min(dp[i],1+dp[i-num[j]]); 
+            }
+        }
+    }
+
+    if(dp[x] == INT_MAX) return -1;
+    return dp[x];
+
+}
