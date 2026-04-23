7cbcc3aef20f2b210f13d1414523b1c8d43109f0
Jayant962 <jayantdhingra370@gmail.com>
2026-04-23 09:17:53 +0530

    Longest common subsequence

--- dp.cpp
+++ dp.cpp
@@ -1147,3 +1147,96 @@ public:
         return solve(prices,fee);
     }
 };
+
+
+
+
+
+
+////////////////     Longest common subsequence
+
+// class Solution {
+// public:
+//     int solve(string text1,string text2,int i,int j){
+//         if(i>=text1.size() || j>=text2.size()) return 0;
+//         if(text1[i]==text2[j]){
+//             return 1+solve(text1,text2,i+1,j+1);
+//         }
+//         else return max(solve(text1,text2,i+1,j),solve(text1,text2,i,j+1));
+//     }
+
+//     int longestCommonSubsequence(string text1, string text2) {
+//         return solve(text1,text2,0,0);
+//     }
+// };
+
+
+// class Solution {
+// public:
+//     int solve(string text1,string text2,int i,int j,vector<vector<int> > &dp){
+//         if(i>=text1.size() || j>=text2.size()) return 0;
+//         if(dp[i][j]!=-1) return dp[i][j];
+//         if(text1[i]==text2[j]){
+//             dp[i][j] = 1+solve(text1,text2,i+1,j+1,dp);
+//         }
+//         else dp[i][j] = max(solve(text1,text2,i+1,j,dp),solve(text1,text2,i,j+1,dp));
+//         return dp[i][j];
+//     }
+
+//     int longestCommonSubsequence(string text1, string text2) {
+//         vector<vector<int> > dp(text1.size(),vector<int>(text2.size(),-1));
+//         return solve(text1,text2,0,0,dp);
+//     }
+// };
+
+
+
+
+// class Solution {
+// public:
+//     int solve(string text1,string text2){
+//         vector<vector<int> > dp(text1.size()+1,vector<int>(text2.size()+1,0));
+//         for(int i=text1.size()-1;i>=0;i--){
+//             for(int j=text2.size()-1;j>=0;j--){
+//                 if(text1[i]==text2[j]){
+//                     dp[i][j] = 1+dp[i+1][j+1];
+//                 }
+//                 else{
+//                     dp[i][j] = max(dp[i+1][j],dp[i][j+1]);
+//                 }
+//             }
+//         }
+//         return dp[0][0];
+//     }
+
+//     int longestCommonSubsequence(string text1, string text2) {
+//         return solve(text1,text2);
+//     }
+// };
+
+
+
+class Solution {
+public:
+    int solve(string text1,string text2){
+        vector<int> curr(text2.size()+1,0);             //   convert dp[i+1]  with next   and dp[i]  with curr
+        vector<int> next(text2.size()+1,0);             //    i is for row j (text2.size) is for col now we only need col therefore text2.size() in both.
+
+        for(int i=text1.size()-1;i>=0;i--){
+            for(int j=text2.size()-1;j>=0;j--){
+                if(text1[i]==text2[j]){
+                    curr[j] = 1+next[j+1];
+                }
+                else{
+                    curr[j] = max(next[j],curr[j+1]);
+                }
+            }
+            next = curr;
+        }
+        return curr[0];
+    }
+
+    int longestCommonSubsequence(string text1, string text2) {
+        return solve(text1,text2);
+    }
+};
